1df6d5d7cb9d63c1c7d9fc5642b61491eb8adcdb
MelodicTechno <617179025@qq.com>
2024-12-24 18:53:22 +0800

    add merge

--- fenzhi/merge_sort.cpp
+++ fenzhi/merge_sort.cpp
@@ -3,40 +3,44 @@ using namespace std;
 
 template<typename T>
 void merge(T a[], int l, int m, int r) {
-    int n1 = r - m + 1;
-    int n2 = m - l;
+    int n1 = m - l + 1; // 左子数组的大小
+    int n2 = r - m;     // 右子数组的大小
 
-    T R[n1];
-    T L[n2];
+    T L[n1]; // 左子数组
+    T R[n2]; // 右子数组
+
+    // 拷贝数据到临时数组L和R
     for (int i = 0; i < n1; i++) {
-        R[i] = a[l + i];
+        L[i] = a[l + i];
     }
-    for (int i = 0; i < n2; i++) {
-        L[i] = a[m + 1 + i];
+    for (int j = 0; j < n2; j++) {
+        R[j] = a[m + 1 + j];
     }
 
-    int i = 0;
-    int j = 0;
-    int k = l;
+    int i = 0; // 左子数组的初始索引
+    int j = 0; // 右子数组的初始索引
+    int k = l; // 合并后数组的初始索引
 
+    // 合并临时数组回到原数组a
     while (i < n1 && j < n2) {
         if (L[i] < R[j]) {
             a[k] = L[i];
             i++;
-        }
-        else {
+        } else {
             a[k] = R[j];
             j++;
         }
         k++;
     }
 
+    // 拷贝L[]的剩余元素
     while (i < n1) {
         a[k] = L[i];
         i++;
         k++;
     }
 
+    // 拷贝R[]的剩余元素
     while (j < n2) {
         a[k] = R[j];
         j++;
@@ -48,12 +52,15 @@ template<typename T>
 void mergeSort(T a[], int left, int right) {
     if (left < right) {
         int middle = left + (right - left) / 2;
+
+        // 分别对左右两半进行排序
         mergeSort(a, left, middle);
         mergeSort(a, middle + 1, right);
-        merge<T>(a, left, middle, right);
+
+        // 合并两半
+        merge(a, left, middle, right);
     }
 }
-
 
 int main() {
     int n;
@@ -62,8 +69,9 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> a[i];
     }
-    mergeSort<int>(a, 0, n - 1);
+    mergeSort(a, 0, n - 1); // 注意右边界应该是n - 1
     for (int i = 0; i < n; i++) {
         cout << a[i] << ' ';
     }
+    return 0;
 }
